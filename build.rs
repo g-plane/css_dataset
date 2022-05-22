d0b84d1bcb33e68dde460431ccd51d6f908e068a
Pig Fang <g-plane@hotmail.com>
2022-05-22 14:55:06 +0800

    add CSS properties

--- build.rs
+++ build.rs
@@ -1,5 +1,30 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
+use serde::Deserialize;
+use std::{env, fs};
 
 fn main() -> Result<()> {
+    generate_properties()?;
+
+    Ok(())
+}
+
+fn generate_properties() -> Result<()> {
+    println!("cargo:rerun-if-changed=vendor/css-properties/data/all.json");
+
+    #[derive(Deserialize)]
+    struct Data {
+        properties: Vec<String>,
+    }
+
+    let data = serde_json::from_str::<Data>(&fs::read_to_string(format!(
+        "{}/vendor/css-properties/data/all.json",
+        env::var("CARGO_MANIFEST_DIR")?
+    ))?)?;
+
+    fs::write(
+        format!("{}/css-properties.json", env::var("OUT_DIR")?),
+        serde_json::to_string(&data.properties)?,
+    )?;
+
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,12 @@ pub static FUNCTIONS: Lazy<AHashSet<&'static str>> = Lazy::new(|| {
     serde_json::from_str(json).unwrap()
 });
 
+/// Known CSS properties.
+pub static PROPERTIES: Lazy<AHashSet<&'static str>> = Lazy::new(|| {
+    let json = include_str!(concat!(env!("OUT_DIR"), "/css-properties.json"));
+    serde_json::from_str(json).unwrap()
+});
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,5 +22,12 @@ mod tests {
         assert!(FUNCTIONS.contains("var"));
         assert!(FUNCTIONS.contains("env"));
         assert!(FUNCTIONS.contains("linear-gradient"));
+    }
+
+    #[test]
+    fn test_properties() {
+        assert!(PROPERTIES.contains("padding"));
+        assert!(PROPERTIES.contains("animation"));
+        assert!(PROPERTIES.contains("accent-color"));
     }
 }

8a5d6d861086205da100a2712ea88ea638279353
Pig Fang <g-plane@hotmail.com>
2024-08-05 15:37:48 +0800

    remove rarely used CSS properties shorthand data

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
 use serde::Deserialize;
-use std::{collections::HashMap, env, error::Error, fs, iter};
+use std::{env, error::Error, fs, iter};
 
 fn main() -> Result<(), Box<dyn Error>> {
     generate_properties()?;
-    generate_properties_shorthand()?;
 
     Ok(())
 }
@@ -31,40 +30,6 @@ fn generate_properties() -> Result<(), Box<dyn Error>> {
             )
             .chain(iter::once("]".to_string()))
             .collect::<String>(),
-    )?;
-
-    Ok(())
-}
-
-fn generate_properties_shorthand() -> Result<(), Box<dyn Error>> {
-    println!("cargo:rerun-if-changed=vendor/properties-shorthand.json");
-
-    let data =
-        serde_json::from_str::<HashMap<String, Vec<String>>>(&fs::read_to_string(format!(
-            "{}/vendor/properties-shorthand.json",
-            env::var("CARGO_MANIFEST_DIR")?
-        ))?)?;
-
-    fs::write(
-        format!("{}/css_properties_shorthand.rs", env::var("OUT_DIR")?),
-        iter::once(format!(
-            "{{\nlet mut map = std::collections::HashMap::with_capacity({});\n",
-            data.len()
-        ))
-        .chain(data.into_iter().flat_map(|(key, value)| {
-            iter::once(format!(
-                "map.insert(\"{key}\", {{\nlet mut vec = Vec::with_capacity({});\n",
-                value.len()
-            ))
-            .chain(
-                value
-                    .into_iter()
-                    .map(|property| format!("vec.push(\"{property}\");\n")),
-            )
-            .chain(iter::once("vec\n});".to_string()))
-        }))
-        .chain(iter::once("map\n}".to_string()))
-        .collect::<String>(),
     )?;
 
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,6 @@ pub mod pseudo_elements;
 pub mod tags;
 
 pub use at_rule::AT_RULES;
-use std::{collections::HashMap, sync::LazyLock};
 
 /// Known CSS functions.
 pub static FUNCTIONS: [&str; 645] = include!(concat!(
@@ -22,12 +21,6 @@ pub static FUNCTIONS: [&str; 645] = include!(concat!(
 
 /// Known CSS properties.
 pub static PROPERTIES: [&str; 1225] = include!(concat!(env!("OUT_DIR"), "/css_properties.rs"));
-
-/// Data of properties shorthand.
-///
-/// Copied from <https://github.com/stylelint/stylelint/blob/main/lib/reference/shorthandData.js>.
-pub static PROPERTIES_SHORTHAND: LazyLock<HashMap<&'static str, Vec<&'static str>>> =
-    LazyLock::new(|| include!(concat!(env!("OUT_DIR"), "/css_properties_shorthand.rs")));
 
 #[cfg(test)]
 mod tests {

9527a489b3f0429e1aa9e365f7917294d5646d5c
Pig Fang <g-plane@hotmail.com>
2024-08-05 15:46:09 +0800

    simplify generating CSS properties data

--- build.rs
+++ build.rs
@@ -1,5 +1,4 @@
-use serde::Deserialize;
-use std::{env, error::Error, fs, iter};
+use std::{env, error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
     generate_properties()?;
@@ -9,28 +8,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn generate_properties() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=vendor/css-properties/data/all.json");
-
-    #[derive(Deserialize)]
-    struct Data {
-        properties: Vec<String>,
-    }
-
-    let data = serde_json::from_str::<Data>(&fs::read_to_string(format!(
+    let json = fs::read_to_string(format!(
         "{}/vendor/css-properties/data/all.json",
         env::var("CARGO_MANIFEST_DIR")?
-    ))?)?;
-
+    ))?;
     fs::write(
         format!("{}/css_properties.rs", env::var("OUT_DIR")?),
-        iter::once("[".to_string())
-            .chain(
-                data.properties
-                    .into_iter()
-                    .map(|property| format!("\"{property}\",")),
-            )
-            .chain(iter::once("]".to_string()))
-            .collect::<String>(),
+        json.trim_start_matches('{')
+            .trim_start()
+            .trim_start_matches("\"properties\":")
+            .trim_end()
+            .trim_end_matches('}'),
     )?;
-
     Ok(())
 }
